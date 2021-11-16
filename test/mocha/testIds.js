17f188ba9df7f617d610f8517f1f14159130f7e0
Ulf Rüegg <uruurumail@gmail.com>
2021-11-16 20:41:12 +0100

    Migrated to EPL-2.0

--- test/mocha/testIds.js
+++ test/mocha/testIds.js
@@ -1,9 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2017 Kiel University and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License 2.0 
+ * which is available at https://www.eclipse.org/legal/epl-2.0/ 
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
