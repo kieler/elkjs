6ba9d6a892ed35803b7d9773b6919b13824c85ad
Ulf Rüegg <uruurumail@gmail.com>
2021-11-16 20:41:12 +0100

    Migrated to EPL-2.0

--- src/js/main-node.js
+++ src/js/main-node.js
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2021 Kiel University and others.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *******************************************************************************/
 const ELK = require('./elk-api.js').default
 
 class ELKNode extends ELK {
