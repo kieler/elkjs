22e4e1358e31e3077ed8b3acf40661048c810288
Jona898 <jonathan.schaeberle@outlook.de>
2022-10-25 10:45:17 +0200

    Changed Space Incanation to 4 for less differences

--- GenerateTypings/LayoutOptions.js
+++ GenerateTypings/LayoutOptions.js
@@ -1,66 +1,62 @@
 import ElkConstructor from "../../lib/main.js";
 
 export class GenerateTypesForApi {
-  constructor() {}
-  elk = new ElkConstructor({});
+    constructor() { }
+    elk = new ElkConstructor({});
 
-  getInterfaceName(baseInterface, specializedName) {
-    return (
-      baseInterface +
-      specializedName.charAt(0) +
-      specializedName.slice(1).toLowerCase()
-    );
-  }
+    getInterfaceName(baseInterface, specializedName) {
+        return (
+            baseInterface +
+            specializedName.charAt(0) +
+            specializedName.slice(1).toLowerCase()
+        );
+    }
 
-  async GenerateLayoutOptionsTypes() {
-    const elkKnownOptions = await this.elk.knownLayoutOptions();
-    const layoutOptions_str = "LayoutOptions";
+    async GenerateLayoutOptionsTypes() {
+        const elkKnownOptions = await this.elk.knownLayoutOptions();
+        const layoutOptions_str = "LayoutOptions";
 
-    const targets = new Set();
-    elkKnownOptions.forEach((element) => {
-      element.targets?.forEach((target) => targets.add(target));
-    });
+        const targets = new Set();
+        elkKnownOptions.forEach((element) => {
+            element.targets?.forEach((target) => targets.add(target));
+        });
 
-    let generatedCode = "";
+        let generatedCode = "";
 
-    targets.forEach((target) => {
-      generatedCode += `export interface ${this.getInterfaceName(
-        layoutOptions_str,
-        target
-      )} {
+        targets.forEach((target) => {
+            generatedCode += `export interface ${this.getInterfaceName(
+                layoutOptions_str,
+                target
+            )} {
         ${elkKnownOptions
-          .filter((value) => value.targets?.includes(target))
-          .map(
-            (val) =>
-              `/** ${val.name}: \n* ${val.description}\n*/\n"${val.id?.slice(
-                "org.eclipse.".length
-              )}"?: ${
-                val.type == "STRING"
-                  ? "string"
-                  : val.type == "DOUBLE"
-                  ? "number"
-                  : val.type == "INT"
-                  ? "number"
-                  : val.type == "BOOLEAN"
-                  ? "boolean"
-                  : "any"
-              }`
-          )
-          .join("\n")}
+                    .filter((value) => value.targets?.includes(target))
+                    .map(
+                        (val) =>
+                            `/** ${val.name}: \n* ${val.description}\n*/\n"${val.id?.slice(
+                                "org.eclipse.".length
+                            )}"?: ${val.type == "STRING"
+                                ? "string"
+                                : val.type == "DOUBLE"
+                                    ? "number"
+                                    : val.type == "INT"
+                                        ? "number"
+                                        : val.type == "BOOLEAN"
+                                            ? "boolean"
+                                            : "any"
+                            }`
+                    )
+                    .join("\n")}
           
         
         [key: string]: any
       }\n\n`;
-    });
+        });
 
-    generatedCode += `export interface ${layoutOptions_str} extends ${
-      [...targets].map <
-      string >
-      ((target) => this.getInterfaceName(layoutOptions_str, target)).join(", ")
-    } {
+        generatedCode += `export interface ${layoutOptions_str} extends ${[...targets]
+            .map < string > ((target) =>
+                this.getInterfaceName(layoutOptions_str, target)).join(", ")
+            } { }`;
 
-    }`;
-
-    return generatedCode;
-  }
+        return generatedCode;
+    }
 }
