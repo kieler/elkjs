58577fa8339fccde7cdeccded8220382ce49f6af
Aaron Paterson <apaterson@pm.me>
2026-04-16 22:27:32 +0000

    restore disco layout algorithm
    DisCo is a polyomino-packing algorithm for arranging already-laid-out
    disconnected subgraphs into a compact whole. It preserves internal
    layouts and only reflows the relative positions of the components.
    This is what a graph with both connected (layered-suited) and
    disconnected subgraphs needs to minimize total area; rectpacking only
    sees bounding boxes and misses concavities in the connected result.
    
    Upstream removed DisCo in PR #288 because `The type CompactionStrategy
    in Disco.melk cannot be resolved` flaked non-deterministically in
    GitHub Actions (issue #291, still open, help-wanted since 2024-07-04).
    The maintainer noted it builds fine locally: "If I keep re-running the
    failed runs, eventually they succeed" and "I can still build it
    locally, I don't think we should jump straight to removing it
    completely." Our consumers do not run CI; we build locally.
    
    While here, bump xtext-gradle-plugin 3.0.2 -> 4.0.0 so the build works
    on Gradle 8 (3.0.2 uses the removed IncrementalTaskInputs API).
    Fileextension -> fileExtensions (SetProperty) and xtendOutputDir ->
    xtend.outputDir are the two required DSL renames.

--- src/java/org/eclipse/elk/js/ElkJs.java
+++ src/java/org/eclipse/elk/js/ElkJs.java
@@ -24,8 +24,7 @@ import org.eclipse.elk.graph.*;
 import org.eclipse.elk.graph.json.*;
 
 import org.eclipse.elk.alg.common.compaction.options.*;
-// FIXME: DisCo breaks Github Actions for unknown reasons, see https://github.com/kieler/elkjs/issues/291
-//import org.eclipse.elk.alg.disco.options.*;
+import org.eclipse.elk.alg.disco.options.*;
 import org.eclipse.elk.alg.layered.options.*;
 import org.eclipse.elk.alg.force.options.*;
 import org.eclipse.elk.alg.mrtree.options.*;
@@ -145,9 +144,8 @@ public class ElkJs implements EntryPoint {
                 SERVICE.registerLayoutMetaDataProviders(new MrTreeMetaDataProvider());
             } else if (alg.equals("radial")) {
                 SERVICE.registerLayoutMetaDataProviders(new RadialMetaDataProvider());
-                // FIXME: DisCo breaks Github Actions for unknown reasons, see https://github.com/kieler/elkjs/issues/291
-   //         } else if (alg.equals("disco")) {
-   //             SERVICE.registerLayoutMetaDataProviders(new PolyominoOptions(), new DisCoMetaDataProvider());
+            } else if (alg.equals("disco")) {
+                SERVICE.registerLayoutMetaDataProviders(new PolyominoOptions(), new DisCoMetaDataProvider());
             } else if (alg.equals("sporeOverlap") || alg.equals("sporeCompaction")) {
                 SERVICE.registerLayoutMetaDataProviders(new SporeMetaDataProvider());
             } else if (alg.equals("rectpacking")) {
