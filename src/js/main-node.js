e15664e1289a2ab9bd934667f92fc8d0e3b04d8f
Max Kasperowski <mka@informatik.uni-kiel.de>
2026-07-08 13:51:49 +0200

    update missing license headers

--- src/js/main-node.js
+++ src/js/main-node.js
@@ -4,7 +4,12 @@
  * terms of the Eclipse Public License 2.0 which is available at
  * https://www.eclipse.org/legal/epl-2.0.
  * 
- * SPDX-License-Identifier: EPL-2.0
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the Eclipse
+ * Public License v. 2.0 are satisfied: GPL-3.0 which is available at
+ * https://www.gnu.org/licenses/gpl-3.0-standalone.html.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR GPL-3.0-or-later
  *******************************************************************************/
 const ELK = require('./elk-api.js').default
 
